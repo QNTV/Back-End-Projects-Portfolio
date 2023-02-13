20dc06fdf7169f5d85e287764168f384b52f0064
Quang Nguyen <quangn8888@gmail.com>
2023-02-13 13:41:23 -0800

    update

---
+++ Link Finder.py
@@ -0,0 +1,27 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+import os
+import csv
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+with open('sites.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        site = row[0]
+        response = requests.get(site)
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        a_tags = soup.find_all('a')
+        print(a_tags)
+
+        urls = []
+        for a in a_tags:
+            if 'href' in a.attrs and a['href'].startswith("/threads/"):
+                urls.append("https://rphangx.net" + a['href'])
+
+        with open('result.csv', 'a', newline='') as f:
+            writer = csv.writer(f)
+            for url in urls:
+                writer.writerow([url])
