e77eea799320c0b61c426879203788008ae9987c
Quang Nguyen <quangn8888@gmail.com>
2023-02-13 16:32:43 -0800

    update

--- Image Finder.py
+++ Image Finder.py
@@ -9,29 +9,30 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 with open('sites.csv', 'r') as file:
     reader = csv.reader(file)
     for row in reader:
-        site = row[0]
-        response = requests.get(site)
+        if row:
+            site = row[0]
+            response = requests.get(site)
 
-        soup = BeautifulSoup(response.text, 'html.parser')
-        img_tags = soup.find_all('img')
-        print(img_tags)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            img_tags = soup.find_all('img')
+            print(img_tags)
 
-        urls = [img['src'] for img in img_tags]
+            urls = [img['src'] for img in img_tags]
 
-        for url in urls:
-            filename = re.search(r'picatt.52av.one/attachments/', url)
-            #filename = re.search(r'https://picatt.52av.one/attachments/*\.jpg$', url)
-            #filename = re.search(r'/([\w_-]+[.](jpg|png|jpeg))$', url)
-            print(filename)
-            if not filename:
-                 print("Regex didn't match with the url: {}".format(url))
-                 continue
-            with open(os.path.join(current_directory, filename.group(1)), 'wb') as f:
-                if 'http' not in url:
-                    # sometimes an image source can be relative 
-                    # if it is provide the base url which also happens 
-                    # to be the site variable atm. 
-                    url = '{}{}'.format(site, url)
-                response = requests.get(url)
-                if len(response.content) >= 10000:
-                    f.write(response.content)
+            for url in urls:
+                #filename = re.search(r'picatt.52av.one/attachments/', url)
+                #filename = re.search(r'https://picatt.52av.one/attachments/*\.jpg$', url)
+                filename = re.search(r'/([\w_-]+[.](jpg|png|jpeg))$', url)
+                print(filename)
+                if not filename:
+                    print("Regex didn't match with the url: {}".format(url))
+                    continue
+                with open(os.path.join(current_directory, filename.group(1)), 'wb') as f:
+                    if 'http' not in url:
+                        # sometimes an image source can be relative 
+                        # if it is provide the base url which also happens 
+                        # to be the site variable atm. 
+                        url = '{}{}'.format(site, url)
+                    response = requests.get(url)
+                    if len(response.content) >= 61900:
+                        f.write(response.content)
